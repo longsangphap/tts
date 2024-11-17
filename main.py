67aa918aa2719070c9352038a9941c2e05e3432e
Nguyễn Doãn hoàng Long <93041814+longsangphap@users.noreply.github.com>
2024-11-17 13:45:37 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, jsonify, send_file
 from gtts import gTTS
-import os
+import io
 
 app = Flask(__name__)
-audio_folder = "audio"
-if not os.path.exists(audio_folder):
-    os.makedirs(audio_folder)
-# API để nhận văn bản và chuyển thành MP3
+
 @app.route('/convert', methods=['POST'])
 def convert_text_to_speech():
     data = request.get_json()
@@ -15,14 +12,16 @@ def convert_text_to_speech():
     if not text:
         return jsonify({"error": "No text provided"}), 400
 
-    # Chuyển văn bản thành giọng nói
+    # Chuyển văn bản thành giọng nói và lưu vào bộ nhớ (chưa lưu vào tệp)
     tts = gTTS(text, lang='vi')
 
-    # Lưu file MP3
+    # Tạo một buffer trong bộ nhớ để lưu tệp MP3
+    mp3_file = io.BytesIO()
+    tts.save(mp3_file)
+    mp3_file.seek(0)  # Đặt lại con trỏ tệp về đầu
 
-    mp3_filename = os.path.join(audio_folder, "output.mp3")
-    tts.save(mp3_filename)
+    # Trả về file MP3 dưới dạng đính kèm
+    return send_file(mp3_file, mimetype='audio/mpeg', as_attachment=True, attachment_filename='output.mp3')
 
-    return send_file(mp3_filename, as_attachment=True)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
